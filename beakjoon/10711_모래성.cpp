9b9768394b5aecdcc3c2877fedbfc2ff5a0ae4f2
leeys <32157701+dmzld@users.noreply.github.com>
2020-03-02 17:16:51 +0900

    Update 10711_모래성.cpp

--- beakjoon/10711_모래성.cpp
+++ beakjoon/10711_모래성.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
-#include <queue>
+#include <vector>
 
 using namespace std;
 
 int H, W;
 char map[1000][1000];
-queue<pair<int, int>> q;
-int dy[8] = { -1, -1, -1, 0, 1, 1, 1, 0};
+bool check[1000][1000] = { false, };
+vector<pair<int, int>> v;
+int dy[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
 int dx[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
 int ans = 0;
+
+int adj(int y, int x){
+	int cnt = 0;
+	for (int i = 0; i < 8; i++){
+		if (map[y + dy[i]][x + dx[i]] == '.')
+			cnt++;
+	}
+	return cnt;
+}
 
 int main(){
 
@@ -23,59 +33,32 @@ int main(){
 	for (int i = 0; i < H; i++){
 		for (int j = 0; j < W; j++){
 			if (map[i][j] != '.'){
-				int cnt = 0;
-				for (int k = 0; k < 8; k++){
-					int cy = i + dy[k], cx = j + dx[k];
-					
-					if (cy < 0 || cy >= H || cx < 0 || cx >= W)
-						continue;
-					if (map[cy][cx] == '.')
-						cnt++;
-				}
-				if (cnt >= map[i][j] - '0'){
-					q.push({ i, j });
+				if (adj(i,j) >= map[i][j] - '0'){ // 처음에 부서질 곳
+					check[i][j] = true;
+					v.push_back({ i, j });
 				}
 			}
 		}
 	}
 
-	while (!q.empty()){
+	while (!v.empty()){
 		ans++;
-		queue<pair<int, int>> q2;
-		bool check[1000][1000] = { false, };
+		vector<pair<int, int>> v2;
 
-		while(!q.empty()){ // 부순다
-			int y = q.front().first, x = q.front().second;
-			q.pop();
-			map[y][x] = '.';
+		for (int i = 0; i < v.size(); i++)
+			map[v[i].first][v[i].second] = '.';
 
-			for (int i = 0; i < 8; i++){
-				int cy = y + dy[i], cx = x + dx[i]; // 부서진 곳의 주변 모래성
-				if (cy >= 0 && cy < H && cx >= 0 && cx < W
-					&& map[cy][cx] != '.' && !check[cy][cx]){
+		for (int i = 0; i < v.size(); i++){
+			int y = v[i].first, x = v[i].second;
+			for (int j = 0; j < 8; j++){
+				int cy = y + dy[j], cx = x + dx[j];
+				if (map[cy][cx] != '.' && adj(cy, cx) >= map[cy][cx] - '0' && !check[cy][cx]){
 					check[cy][cx] = true;
-					q2.push({ cy, cx });
+					v2.push_back({ cy, cx });
 				}
 			}
 		}
-
-		while (!q2.empty()){ // 부순 뒤 상황에서
-			int y = q2.front().first, x = q2.front().second; // 부서진 곳의 주변 모래성
-			q2.pop();
-			int cnt = 0;
-			
-			for (int j = 0; j < 8; j++){
-				int cy = y + dy[j], cx = x + dx[j];
-				
-				if (cy < 0 || cy >= H || cx < 0 || cx >= W)
-					continue;
-				if (map[cy][cx] == '.')
-					cnt++;
-			}
-
-			if (cnt >= map[y][x] - '0')
-				q.push({ y, x }); // 조건이 바뀐 모래성들
-		}
+		v = v2;
 	}
 
 

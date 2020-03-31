9d97d27ac4ad5d4e1eb7ccda65c31d5e43f229de
leeys <32157701+dmzld@users.noreply.github.com>
2020-03-31 19:19:38 +0900

    Create 8741_두문자어.cpp

---
+++ SWEA/8741_두문자어.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int TC;
+
+int main(){
+
+	cin >> TC;
+	cin.ignore(); // point
+
+	for (int tc = 1; tc <= TC; tc++){
+		string str;
+		int idx = 0;
+		char ans[3];
+
+		getline(cin, str);
+
+		ans[idx++] = str.at(0) - 32;
+		for (int i = 1; i < str.length(); i++){
+			if (str.at(i) == ' '){
+				ans[idx++] = str.at(i + 1) - 32;
+			}
+
+			if (idx >= 3)
+				break;
+		}
+
+		cout << "#" << tc << " ";
+		for (int i = 0; i < 3; i++)
+			cout << ans[i];
+		cout << "\n";
+	}
+
+	return 0;
+}

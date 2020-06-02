bc763255d4666e5acd7abe8299261f7a948bdee9
leeys <32157701+dmzld@users.noreply.github.com>
2020-06-02 17:00:34 +0900

    Update 5014_스타트링크.cpp

--- BOJ/5014_스타트링크.cpp
+++ BOJ/5014_스타트링크.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
-#include <cmath>
 #include <vector>
+#include <queue>
 
 using namespace std;
 
-int F, S, G, U, D; // 건물층, 현재층, 스타트링크층, up버튼, down버튼
-bool check[1000001];
-int ans = 0;
+int F, G, S, updown[2], check[1000001] = { 0, };
+queue<int> q;
 
 int main(){
 
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
-	cin >> F >> S >> G >> U >> D;
+	cin >> F >> S >> G >> updown[0] >> updown[1];
+	updown[1] = -updown[1];
 
-	while (S != G){
-		if (check[S])
-			break;
-		else
-			check[S] = true;
-		
-		ans++;
+	check[S] = 1;
+	q.push(S);
+	while (!q.empty()){
+		int qSize = q.size();
+		for (int i = 0; i < qSize; i++){
+			int cs = q.front();
+			q.pop();
 
-		if (G > S)
-			S += U;
-		else
-			S -= D;
+			if (cs == G){
+				cout << check[cs] - 1;
+				return 0;
+			}
+
+			for (int i = 0; i < 2; i++){
+				int ns = cs + updown[i];
+
+				if (ns<1 || ns>F || check[ns] > 0) continue;
+
+				check[ns] = check[cs] + 1;
+				q.push(ns);
+			}
+		}
 	}
 
-	if (check[S])
-		cout << "use the stairs";
-	else
-		cout << ans;
+	cout << "use the stairs";
 
 	return 0;
 }

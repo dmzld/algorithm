f3d1e1e71313f22653169eb5f99cc2aea8af2d08
leeys <32157701+dmzld@users.noreply.github.com>
2020-06-02 11:25:23 +0900

    Update 1976_여행가자.cpp

--- BOJ/1976_여행가자.cpp
+++ BOJ/1976_여행가자.cpp
@@ -7,18 +7,18 @@ using namespace std;
 int N, M, G[201][201], route[1000];
 int conn[201] = { 0, };
 
-int findParent(int n){
-	int pN = conn[n];
-	while (pN != conn[pN])
-		pN = conn[pN];
-	return pN;
+int findHead(int n){
+	int hN = conn[n];
+	while (hN != conn[hN])
+		hN = conn[hN];
+	return hN;
 }
 
 bool arrive(){
 	bool arrive = true;
 
 	for (int i = 0; i < M - 1; i++)
-		if (conn[route[i]] != conn[route[i + 1]]){
+		if (findHead(route[i]) != findHead(route[i + 1])){
 			arrive = false;
 			break;
 		}
@@ -39,14 +39,19 @@ int main(){
 	for (int i = 0; i < M; i++)
 		cin >> route[i];
 
-	for (int i = 1; i <= N; i++){
-    // need to fix
-		// head city of union is the smallest number city
+	for (int i = 1; i <= N; i++)
 		conn[i] = i;
+
+	for (int i = 1; i <= N; i++){
+		// head city of union is the smallest number city
 		for (int j = 1; j <= N; j++){
 			if (G[i][j] == 1){ 
-				int p = findParent(j);
-				conn[i] = conn[i] < p ? conn[i] : p;
+				int p = findHead(i),
+				    q = findHead(j);
+
+				// connect head to head
+				if (p < q) conn[q] = p;
+				else conn[p] = q;
 			}
 		}
 	}

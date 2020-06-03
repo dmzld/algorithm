acce8aec27d7ed776101cb3b5d0093d1ea36f6e6
leeys <32157701+dmzld@users.noreply.github.com>
2020-06-03 14:55:02 +0900

    Update 9466_팀프로젝트.cpp

--- BOJ/9466_팀프로젝트.cpp
+++ BOJ/9466_팀프로젝트.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <vector>
 #include <cstring>
 
 using namespace std;
 
-int T, n, choice[100001], cnt;
-bool check[1000001], done[1000001] = { false, };
+int T, n, choice[100001], cnt= 0;
+bool check[100001], done[100001];
 
-void solution(int num){
+void dfs(int num){
 	check[num] = true;
 
-	int nnum = choice[num];
+	int nnum = choice[num]; // ** point 1
 
-	if (!check[nnum] && !done[nnum])
-		solution(nnum);
-	else if (check[nnum] && !done[nnum]){
-		for (int i = nnum; i != num; i = choice[i]) // ** range is point to shorten the time
-			if (check[i]) cnt++;
-		cnt++;
+	if (check[nnum] && !done[nnum]){ // find cycle
+		for (int i = choice[nnum]; i != nnum; i = choice[i]) // ** point 2
+			cnt++;
+		cnt++; // itself
 	}
+	else if (!check[nnum])
+		dfs(nnum);
 
 	done[num] = true;
 }
@@ -29,19 +28,18 @@ int main(){
 	cin.tie(0);
 
 	cin >> T;
-	for (int tc = 0; tc < T; tc++){		
+	for (int tc = 0; tc < T; tc++){
 		cin >> n;
-
-		memset(check, false, sizeof(check));
-		memset(done, false, sizeof(done));
-		cnt = 0;
-
 		for (int i = 1; i <= n; i++)
 			cin >> choice[i];
 
-		for (int i = 1; i <= n; i++){
-			if (!done[i]) solution(i);
-		}
+		cnt = 0;
+		memset(check, false, sizeof(check));
+		memset(done, false, sizeof(done));
+
+		for (int i = 1; i <= n; i++)
+			if (!done[i])
+				dfs(i);
 
 		cout << n - cnt << "\n";
 	}

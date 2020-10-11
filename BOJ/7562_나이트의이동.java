a8dda9f7e8ff9add71fd1e1a364420851417a030
leeys <32157701+dmzld@users.noreply.github.com>
2020-10-12 00:00:10 +0900

    Update and rename 7562.java to 7562_나이트의이동.java

--- BOJ/7562_나이트의이동.java
+++ BOJ/7562_나이트의이동.java
@@ -1,12 +1,10 @@
-package temp;
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Algorithms {
+public class Main {
 
 	public static class Info{
 		int y, x;
